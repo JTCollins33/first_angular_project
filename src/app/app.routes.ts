11f53d7764c747e402ed09eacfccc85b5c1cacff
JTCollins33 <JTCollinsy99@gmail.com>
2025-02-21 13:53:50 -0500

    added routing functionality through header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: '1',
+        component: Page1Component
+    },
+    {
+        path: '2',
+        component: Page2Component
     }
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,24 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   title = input('Default title');
 
-  navigateHome = ()=>{
-    console.log("home")
-  }
+  // navigateHome = ()=>{
+  //   console.log("home")
+  // }
 
-  navigatePage1 = ()=>{
-    console.log("page1")
-  }
+  // navigatePage1 = ()=>{
+  //   console.log("page1")
+  // }
 
-  navigatePage2 = ()=> [
-    console.log("page2")
-  ]
+  // navigatePage2 = ()=> [
+  //   console.log("page2")
+  // ]
 }
