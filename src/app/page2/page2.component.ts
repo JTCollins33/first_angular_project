d4946dce36ca177e3e9ccb8a4967f0cdedd83676
JTCollins33 <JTCollinsy99@gmail.com>
2025-02-25 14:33:08 -0500

    added directives to page2

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page2',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './page2.component.html',
   styleUrl: './page2.component.scss'
 })
 export class Page2Component {
-  ngOnInit(): void {
-    console.log("page2 loaded")
+  isVisible = true;
+  randomList=['1','2','3']
+  switchNumber=1
+
+  incrementSwitchNumber(){
+    this.switchNumber+=1
+  }
+  decrementSwitchNumber(){
+    this.switchNumber-=1
   }
 }
