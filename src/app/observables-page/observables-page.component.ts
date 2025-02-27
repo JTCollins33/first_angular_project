c014195692e6ecdc5a53aa03b951550ae14bd61d
JTCollins33 <JTCollinsy99@gmail.com>
2025-02-27 10:07:02 -0500

    added outline for observables page

---
+++ src/app/observables-page/observables-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-observables-page',
+  imports: [],
+  templateUrl: './observables-page.component.html',
+  styleUrl: './observables-page.component.scss'
+})
+export class ObservablesPageComponent {
+
+}
